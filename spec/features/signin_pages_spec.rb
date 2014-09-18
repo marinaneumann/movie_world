84a24f282ff670a139da96f80a81a5534489fad0
Marina Neumann <marinaneuamnn2468@yahoo.com>
2014-09-18 11:25:30 -0700

    Allow a user to sign up, deny a user the bility to sign up if fields are incomplete, and will successfully log out a user.

--- spec/features/signin_pages_spec.rb
+++ spec/features/signin_pages_spec.rb
@@ -19,4 +19,33 @@ describe 'thesignin process' do
     expect(page). to have_content 'Log In'
   end
 
+    it "allows a user to sign up" do
+      visit '/signup'
+      fill_in 'Email', with: 'user@email.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Password confirmation', with: 'password'
+      click_button 'Sign Up'
+      expect(page).to have_content 'Hi'
+    end
+
+    it "will not allow a user to sign up" do
+      visit '/signup'
+      fill_in 'Email', with: 'user@email.com'
+      fill_in 'Password', with: 'password'
+      fill_in 'Password confirmation', with: ''
+      click_button 'Sign Up'
+      expect(page).to have_content 'Access Denied'
+    end
+
+
+    it "will successfully log out a user" do
+      user1 = User.create(email: 'user@email.com', password: 'testpassword', :password_confirmation => 'testpassword')
+      visit '/login'
+      fill_in 'Email', with: 'user@email.com'
+      fill_in 'Password', with: 'testpassword'
+      click_button 'Log In'
+      click_link 'Log Out'
+      expect(page).to have_content 'Hi'
+    end
+
 end
