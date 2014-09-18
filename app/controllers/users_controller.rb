bca48903118de322c76b2460b30a7c7ae756530a
Marina Neumann <marinaneuamnn2468@yahoo.com>
2014-09-18 11:17:20 -0700

    Establish ability for user to sign up and log in.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
-class UsersContrller < ApplicationController
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
+  root to: 'users#index'
   resources :users
   resources :sessions
 end
