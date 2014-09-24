f844376011409034d3e7754b6e6a8331ffd6de21
Marina Neumann <marinaneumann2468@yahoo.com>
2014-09-24 10:46:01 -0700

    Establish association that a movie belongs to a user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, :password, :password_confirmation, :presence => true
   validates_uniqueness_of :email
-
   has_many :movies
 end
